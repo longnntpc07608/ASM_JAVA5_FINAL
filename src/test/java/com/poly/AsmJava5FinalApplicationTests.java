298e71d95eedb9e8ef0b7b08115e42f69b5988e3
longnntpc07608 <150583615+longnntpc07608@users.noreply.github.com>
2024-06-19 20:20:22 +0700

    Add files via upload

---
+++ src/test/java/com/poly/AsmJava5FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmJava5FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
